c785ce31ab2b2a032676bb8443f0101e8c4a05ea
Antoine Jaussoin <antoine@jaussoin.com>
2020-10-07 18:22:16 +0100

    Separating editor from creator

---
+++ retro-board-app/src/views/session-editor/MaxVoteSlider.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import Slider, { Mark } from '@material-ui/core/Slider';
+
+interface MaxVoteSliderProps {
+  value: number | null;
+  onChange: (value: number | null) => void;
+}
+
+const UNLIMITED = 6; // Arbitrary value, translated to null anyway
+
+const marks: Mark[] = [
+  { value: 0, label: '0' },
+  { value: 1, label: '1' },
+  { value: 2, label: '2' },
+  { value: 3, label: '3' },
+  { value: 4, label: '4' },
+  { value: 5, label: '5' },
+  { value: UNLIMITED, label: 'Unlimited' },
+];
+
+const MaxVoteSlider = ({ value, onChange }: MaxVoteSliderProps) => {
+  const handleChange = useCallback(
+    (_event: React.ChangeEvent<{}>, value: number | number[]) =>
+      onChange(value === UNLIMITED ? null : (value as number)),
+    [onChange]
+  );
+  return (
+    <Container>
+      <Slider
+        value={value === null ? UNLIMITED : (value as number)}
+        onChange={handleChange}
+        marks={marks}
+        min={0}
+        max={UNLIMITED}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding-right: 20px;
+`;
+
+export default MaxVoteSlider;
---
+++ retro-board-app/src/views/session-editor/SessionEditor.tsx
@@ -0,0 +1,293 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { SessionOptions } from 'retro-board-common';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  useMediaQuery,
+  DialogActions,
+  FormControlLabel,
+  Checkbox,
+  Button,
+} from '@material-ui/core';
+import SettingCategory from '../home/components/SettingCategory';
+import useTranslations from '../../translations';
+import useToggle from '../../hooks/useToggle';
+import { ColumnSettings, Template } from '../../state/types';
+import { buildDefaults, merge } from '../../state/columns';
+import { trackEvent } from '../../track';
+import { getTemplate } from '../../state';
+import OptionItem from './OptionItem';
+import Slider from './Slider';
+import ColumnEditor from './ColumnEditor';
+import MaxVoteSlider from './MaxVoteSlider';
+import BooleanOption from './BooleanOption';
+import TemplatePicker from './TemplatePicker';
+
+interface SessionEditorProps {
+  open: boolean;
+  options: SessionOptions;
+  columns: ColumnSettings[];
+  onChange: (
+    options: SessionOptions,
+    columns: ColumnSettings[],
+    makeDefault: boolean
+  ) => void;
+  onClose: () => void;
+}
+
+function SessionEditor({
+  open,
+  options,
+  columns,
+  onChange,
+  onClose,
+}: SessionEditorProps) {
+  const translations = useTranslations();
+  const { Customize, Generic } = translations;
+  const fullScreen = useMediaQuery('(max-width:600px)');
+  const [isDefaultTemplate, toggleIsDefaultTemplate] = useToggle(false);
+  const [maxUpVotes, setMaxUpVotes] = useState<number | null>(null);
+  const [maxDownVotes, setMaxDownVotes] = useState<number | null>(null);
+  const [allowActions, setAllowActions] = useState(true);
+  const [allowSelfVoting, setAllowSelfVoting] = useState(false);
+  const [allowMultipleVotes, setAllowMultipleVotes] = useState(false);
+  const [allowAuthorVisible, setAllowAuthorVisible] = useState(false);
+  const [allowGiphy, setAllowGiphy] = useState(true);
+  const [allowGrouping, setAllowGrouping] = useState(true);
+  const [allowReordering, setAllowReordering] = useState(true);
+  const [blurCards, setBlurCards] = useState(false);
+  const [numberOfColumns, setNumberOfColumns] = useState(3);
+  const [defaultDefinitions, setDefaultDefinitions] = useState(
+    buildDefaults('default', translations)
+  );
+
+  const [definitions, setDefinitions] = useState<ColumnSettings[]>(
+    buildDefaults('default', translations).map(
+      (d) =>
+        ({ type: d.type, color: '', icon: null, label: '' } as ColumnSettings)
+    )
+  );
+
+  useEffect(() => {
+    setDefaultDefinitions(buildDefaults('default', translations));
+  }, [translations]);
+
+  useEffect(() => {
+    setAllowActions(options.allowActions);
+    setAllowAuthorVisible(options.allowAuthorVisible);
+    setAllowGiphy(options.allowGiphy);
+    setAllowGrouping(options.allowGrouping);
+    setAllowMultipleVotes(options.allowMultipleVotes);
+    setAllowReordering(options.allowReordering);
+    setAllowSelfVoting(options.allowSelfVoting);
+    setBlurCards(options.blurCards);
+    setMaxDownVotes(options.maxDownVotes);
+    setMaxUpVotes(options.maxUpVotes);
+  }, [options]);
+
+  useEffect(() => {
+    setNumberOfColumns(columns.length);
+    setDefinitions(columns);
+  }, [columns]);
+
+  const handleColumnChange = useCallback(
+    (value: ColumnSettings, index: number) => {
+      setDefinitions((cols) => Object.assign([], cols, { [index]: value }));
+      trackEvent('custom-modal/column/change');
+    },
+    []
+  );
+
+  const handleTemplateChange = useCallback(
+    (templateType: Template) => {
+      const template = buildDefaults(templateType, translations);
+      setNumberOfColumns(getTemplate(templateType, translations).length);
+      setDefaultDefinitions(template);
+      trackEvent('custom-modal/template/select');
+    },
+    [translations]
+  );
+
+  const handleCreate = useCallback(() => {
+    onChange(
+      {
+        allowActions,
+        allowAuthorVisible,
+        allowGiphy,
+        allowGrouping,
+        allowMultipleVotes,
+        allowReordering,
+        allowSelfVoting,
+        blurCards,
+        maxDownVotes,
+        maxUpVotes,
+      },
+      merge(definitions, defaultDefinitions, numberOfColumns),
+      isDefaultTemplate
+    );
+  }, [
+    onChange,
+    allowActions,
+    allowAuthorVisible,
+    allowGiphy,
+    allowGrouping,
+    allowMultipleVotes,
+    allowReordering,
+    allowSelfVoting,
+    blurCards,
+    maxDownVotes,
+    maxUpVotes,
+    defaultDefinitions,
+    definitions,
+    numberOfColumns,
+    isDefaultTemplate,
+  ]);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullWidth
+      fullScreen={fullScreen}
+      keepMounted={false}
+    >
+      <DialogTitle>{Customize.title}</DialogTitle>
+      <DialogContent>
+        <SettingCategory
+          title={Customize.customTemplateCategory!}
+          subtitle={Customize.customTemplateCategorySub!}
+        >
+          <OptionItem
+            label={Customize.template!}
+            help={Customize.templateHelp!}
+          >
+            <TemplatePicker onSelect={handleTemplateChange} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.numberOfColumns!}
+            help={Customize.numberOfColumnsHelp!}
+          >
+            <Slider
+              value={numberOfColumns}
+              onChange={setNumberOfColumns}
+              from={1}
+              to={5}
+            />
+          </OptionItem>
+          <>
+            {definitions.slice(0, numberOfColumns).map((def, index) => (
+              <ColumnEditor
+                key={index}
+                value={def}
+                defaults={defaultDefinitions[index]}
+                onChange={(value) => handleColumnChange(value, index)}
+              />
+            ))}
+          </>
+        </SettingCategory>
+        <SettingCategory
+          title={Customize.votingCategory!}
+          subtitle={Customize.votingCategorySub!}
+        >
+          <OptionItem
+            label={Customize.maxUpVotes!}
+            help={Customize.maxUpVotesHelp!}
+          >
+            <MaxVoteSlider value={maxUpVotes} onChange={setMaxUpVotes} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.maxDownVotes!}
+            help={Customize.maxDownVotesHelp!}
+          >
+            <MaxVoteSlider value={maxDownVotes} onChange={setMaxDownVotes} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowSelfVoting!}
+            help={Customize.allowSelfVotingHelp!}
+          >
+            <BooleanOption
+              value={allowSelfVoting}
+              onChange={setAllowSelfVoting}
+            />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowMultipleVotes!}
+            help={Customize.allowMultipleVotesHelp!}
+          >
+            <BooleanOption
+              value={allowMultipleVotes}
+              onChange={setAllowMultipleVotes}
+            />
+          </OptionItem>
+        </SettingCategory>
+        <SettingCategory
+          title={Customize.postCategory!}
+          subtitle={Customize.postCategorySub!}
+        >
+          <OptionItem
+            label={Customize.allowActions!}
+            help={Customize.allowActionsHelp!}
+          >
+            <BooleanOption value={allowActions} onChange={setAllowActions} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowAuthorVisible!}
+            help={Customize.allowAuthorVisibleHelp!}
+          >
+            <BooleanOption
+              value={allowAuthorVisible}
+              onChange={setAllowAuthorVisible}
+            />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowReordering!}
+            help={Customize.allowReorderingHelp!}
+          >
+            <BooleanOption
+              value={allowReordering}
+              onChange={setAllowReordering}
+            />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowGrouping!}
+            help={Customize.allowGroupingHelp!}
+          >
+            <BooleanOption value={allowGrouping} onChange={setAllowGrouping} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.allowGiphy!}
+            help={Customize.allowGiphyHelp!}
+          >
+            <BooleanOption value={allowGiphy} onChange={setAllowGiphy} />
+          </OptionItem>
+          <OptionItem
+            label={Customize.blurCards!}
+            help={Customize.blurCardsHelp!}
+          >
+            <BooleanOption value={blurCards} onChange={setBlurCards} />
+          </OptionItem>
+        </SettingCategory>
+      </DialogContent>
+      <DialogActions>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={isDefaultTemplate}
+              onChange={toggleIsDefaultTemplate}
+            />
+          }
+          label={Customize.makeDefaultTemplate}
+        />
+        <Button onClick={onClose} color="default" variant="text">
+          {Generic.cancel}
+        </Button>
+        <Button onClick={handleCreate} color="primary" variant="contained">
+          {Customize.startButton}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default SessionEditor;
