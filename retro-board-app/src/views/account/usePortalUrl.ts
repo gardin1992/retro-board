59c8eb049d69cc87ea11dc61ea9a278e29a3848c
Antoine Jaussoin <antoine@jaussoin.com>
2020-11-02 18:25:49 +0000

    console

--- retro-board-app/src/views/account/usePortalUrl.ts
+++ retro-board-app/src/views/account/usePortalUrl.ts
@@ -5,9 +5,7 @@ export default function usePortalUrl(): string | null {
   const [url, setUrl] = useState<string | null>(null);
   useEffect(() => {
     async function fetchUrl() {
-      console.log('abbout to fetch');
       const url = await getPortalUrl();
-      console.log('got url', url);
       setUrl(url);
     }
     fetchUrl();
