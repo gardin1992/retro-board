493e8727207b83104cba87a81fa5daf893d86358
Antoine Jaussoin <antoine@jaussoin.com>
2020-11-03 10:53:58 +0000

    Adding rewired back

--- retro-board-app/config-overrides.js
+++ retro-board-app/config-overrides.js
@@ -1,8 +1,8 @@
-// const rewireWorkspaces = require('react-app-rewire-yarn-workspaces');
-// const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireWorkspaces = require('react-app-rewire-yarn-workspaces');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = function overrideCra(config, env) {
-  // config = rewireWorkspaces(config, env);
-  // config = rewireStyledComponents(config, env);
+  config = rewireWorkspaces(config, env);
+  config = rewireStyledComponents(config, env);
   return config;
 };
