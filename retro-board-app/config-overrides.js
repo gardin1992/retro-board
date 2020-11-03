d09f55e0e4f0e92758059408b0ccb724cd9f5c21
Antoine Jaussoin <antoine@jaussoin.com>
2020-11-03 10:43:45 +0000

    CRA 4 wip

--- retro-board-app/config-overrides.js
+++ retro-board-app/config-overrides.js
@@ -1,10 +1,8 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireWorkspaces = require('react-app-rewire-yarn-workspaces');
-const rewireStyledComponents = require('react-app-rewire-styled-components');
+// const rewireWorkspaces = require('react-app-rewire-yarn-workspaces');
+// const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = function overrideCra(config, env) {
-  config = rewireReactHotLoader(config, env);
-  config = rewireWorkspaces(config, env);
-  config = rewireStyledComponents(config, env);
+  // config = rewireWorkspaces(config, env);
+  // config = rewireStyledComponents(config, env);
   return config;
 };
